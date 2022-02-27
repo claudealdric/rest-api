94df4c0952e3e11ad3185f4d75c8db2cd004688b
claudealdric <cvaldric@gmail.com>
2022-02-27 10:35:39 -0500

    Add ORM module to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import 'dotenv/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './entities/user.entity';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'oregon-postgres.render.com',
+      port: 5432,
+      username: 'claude',
+      password: process.env.DB_PASSWORD,
+      database: 'rest_api',
+      entities: [User],
+      synchronize: true,
+      ssl: { rejectUnauthorized: false },
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
