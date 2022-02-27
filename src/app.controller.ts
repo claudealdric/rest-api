d8d1984e063f4d787be08b7b78d46a7b5720af53
claudealdric <cvaldric@gmail.com>
2022-02-27 09:40:50 -0500

    Create health check endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,10 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/health-check')
+  checkHealth(): string {
+    const now = new Date();
+    return now.toISOString();
+  }
 }
